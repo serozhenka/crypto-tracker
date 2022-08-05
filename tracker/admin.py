8ec6abe94a147a2eebeda745d4440ff9884e661d
Shevchuk serozhenka <sergeykaoff@gmail.com>
2022-08-05 12:13:49 +0300

    added: crypto currency model

---
+++ tracker/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import CryptoCurrency
+
+
+class CryptoCurrencyAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'name', 'rank', 'price', 'market_cap')
---
+++ tracker/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class CryptoCurrency(models.Model):
+    identifier = models.CharField(max_length=16)
+    rank = models.SmallIntegerField()
+    name = models.CharField(max_length=128)
+    logo_url = models.URLField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    market_cap = models.BigIntegerField()
+    circulating_supply = models.BigIntegerField()
+    max_supply = models.BigIntegerField(null=True)
+
+    price_change_pct_1d = models.DecimalField(max_digits=9, decimal_places=4)
+    price_change_pct_7d = models.DecimalField(max_digits=9, decimal_places=4)
+    price_change_pct_30d = models.DecimalField(max_digits=9, decimal_places=4)
+    price_change_pct_365d = models.DecimalField(max_digits=9, decimal_places=4)
+
+    class Meta:
+        ordering = ['rank']
+
+    def __str__(self):
+        return self.identifier
+
+    @property
+    def circulating_supply_ptc(self):
+        if self.max_supply:
+            return self.circulating_supply / self.max_supply * 100
+        return 100
